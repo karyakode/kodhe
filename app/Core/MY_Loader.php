83dc3f447a900fa98002c9f68b1a0a237f2bdcc5
Rohmad Kadarwanto <rohmadkadarwanto@gmail.com>
2024-12-20 05:13:31 +0700

    Initial commit for Kodhe Framework

--- app/Core/MY_Loader.php
+++ app/Core/MY_Loader.php
@@ -5,17 +5,17 @@ class MY_Loader extends \CI_Loader
 
   function __construct()
   {
-    $di = new \Flame\Core\Dependency\DependencyResolver();
+    $di = new \Kodhe\Core\Dependency\DependencyResolver();
 
     parent::__construct(
-      $di->resolve('Flame\Core\Facade\Facade'),
-      $di->resolve('Flame\Core\Loader\Package\Package'),
-      $di->resolve('Flame\Core\Loader\Library\Library'),
-      $di->resolve('Flame\Core\Loader\Model\Model'),
-      $di->resolve('Flame\Core\Loader\Helper\Helper'),
-      $di->resolve('Flame\Core\Loader\View\View'),
-      $di->resolve('Flame\Core\Loader\Driver\Driver'),
-      $di->resolve('Flame\Core\Loader\Autoloader'),
+      $di->resolve('Kodhe\Core\Facade\Facade'),
+      $di->resolve('Kodhe\Core\Loader\Package\Package'),
+      $di->resolve('Kodhe\Core\Loader\Library\Library'),
+      $di->resolve('Kodhe\Core\Loader\Model\Model'),
+      $di->resolve('Kodhe\Core\Loader\Helper\Helper'),
+      $di->resolve('Kodhe\Core\Loader\View\View'),
+      $di->resolve('Kodhe\Core\Loader\Driver\Driver'),
+      $di->resolve('Kodhe\Core\Loader\Autoloader'),
     );
   }
 
